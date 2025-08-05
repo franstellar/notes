3917fd923952950990e8ea69300e56434a89796a
franzoca <franferrarifloriano@gmail.com>
2025-08-05 17:45:47 -0300

    login form

--- routes/web.php
+++ routes/web.php
@@ -2,3 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MainController;
+use App\Http\Controllers\AuthController;
+
+// auth routes
+Route::get('/login', [AuthController::class, 'login']);
+ROute::post('/authenticate', [AuthController::class, 'authenticate']);
+Route::get('/logout', [AuthController::class, 'logout']);
