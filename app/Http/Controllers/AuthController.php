4c9921f3305010eaf6ab815eb4f6e5369f367be9
franzoca <franferrarifloriano@gmail.com>
2025-08-13 17:38:45 -0300

    connection to database

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class AuthController extends Controller
 {
@@ -30,9 +31,13 @@ class AuthController extends Controller
         $username = $request->input('text_username');
         $password = $request->input('text_password');
 
-        echo $request->input('text_username');
-        echo '<br>';
-        echo $request->input('text_password');
+        // test database connection
+        try {
+            DB::connection()->getPdo();
+            echo 'Database connection successful';
+        } catch (\PDOException $e) {
+            echo 'Database connection failed: ' . $e->getMessage();
+        }
     }
 
     public function logout()
